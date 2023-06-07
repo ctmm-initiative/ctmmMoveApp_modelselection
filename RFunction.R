9f710ba03d5bcd9b315c16b0697914b980d3a88c
Marco Sciaini <marco@kaldera.dev>
2023-06-07 20:56:02 +0200

    init

---
+++ RFunction.R
@@ -0,0 +1,13 @@
+library(ctmm)
+library(purrr)
+
+rFunction = function(data) {
+  
+  g <- lapply(data, \(x) ctmm.guess(x, interactive = FALSE))
+  f1 <- map2(data, g, ~ ctmm.select(.x, .y))
+  capture.output(summary(f1), file = appArtifactPath("model_summary.txt"))
+  saveRDS(f1, file = appArtifactPath("model.rds"))
+  
+  return(list(data,f1))
+  
+}
