84b4c36897f0ab76f6ab7b52320ba2a396874c8c
Johannes <jmsigner@gmail.com>
2023-06-26 13:17:20 +0200

    new artifact

--- RFunction.R
+++ RFunction.R
@@ -8,6 +8,13 @@ rFunction = function(data) {
   capture.output(summary(f1), file = appArtifactPath("model_summary.txt"))
   saveRDS(f1, file = appArtifactPath("model.rds"))
   
-  
+  # Return summary
+  xx <- map(f1, ~ summary(.x)$CI)
+  lens <- map_int(xx, nrow)
+  xx <- do.call(rbind, xx)
+  xx |> as_tibble() |> 
+    mutate(id = rep(names(f1), lens), unit = rownames(xx)) |> 
+    select(id, unit, low, est, high) |> 
+    write.csv("ctmm_select_summary.txt")
   return(list(data,f1))
 }
